21f548acd1627f3453137f49b40067cdc4ff43f5
rojandahal <maxpanye60@gmail.com>
2024-08-02 19:42:32 +0545

    fix: cross origin

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const errorHandler = require("./errors/api-error-handler");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const errorHandler = require('./errors/api-error-handler');
 
 // Initializing environment vairables
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
@@ -17,16 +17,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use of cors
 const corsOptions = {
   origin: [
-    "http://localhost:3000",
-    "http://localhost:5000",
-    "http://127.0.0.1:3000",
-    "http://192.168.1.103:3000",
-    "http://192.168.1.74:3000",
-		"http://murphysdemo.com.au/",
-		"http://groovy.murphysdemo.com.au/",
-		"http://murphysdemo.com.au/groovy",
-		"http://groovy.murphysdemo.com.au",
-		"http://murphysdemo.com.au",
+    'http://localhost:3000',
+    'http://localhost:5000',
+    'http://127.0.0.1:3000',
+    'http://192.168.1.103:3000',
+    'http://192.168.1.74:3000',
+    'http://murphysdemo.com.au/',
+    'http://groovy.murphysdemo.com.au/',
+    'http://murphysdemo.com.au/groovy',
+    'http://groovy.murphysdemo.com.au',
+    'http://murphysdemo.com.au',
+    'https://groovyhandmade.onrender.com/',
+    'https://render.com/',
+    'http://groovyhandmade.onrender.com/',
+    'http://render.com/',
   ],
   credentials: true,
   optionSuccessStatus: 200,
@@ -49,25 +53,25 @@ mongoose.connect(uri, {
 });
 const connection = mongoose.connection;
 
-connection.once("open", () => {
-  console.log("Mongoose connection successfully!");
+connection.once('open', () => {
+  console.log('Mongoose connection successfully!');
 });
 
 //Routes files
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const authRoutes = require("./routes/auth");
-const orderRoutes = require("./routes/order");
-const newArrivals = require("./routes/newarrivals");
-const bannerRoutes = require("./routes/banner");
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const authRoutes = require('./routes/auth');
+const orderRoutes = require('./routes/order');
+const newArrivals = require('./routes/newarrivals');
+const bannerRoutes = require('./routes/banner');
 
 //Adding routes middleware
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
-app.use("/api/v1/order", orderRoutes);
-app.use("/api/v1/newarrivals", newArrivals);
-app.use("/api/v1/banner", bannerRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/product', productRoutes);
+app.use('/api/v1/order', orderRoutes);
+app.use('/api/v1/newarrivals', newArrivals);
+app.use('/api/v1/banner', bannerRoutes);
 
 //Error handler
 app.use(errorHandler);
@@ -77,11 +81,11 @@ const port = process.env.PORT || 5000;
 
 //Connecting to server
 const server = app.listen(port, () => {
-  console.log("Created server at port: " + port);
+  console.log('Created server at port: ' + port);
 });
 
 // Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
+process.on('unhandledRejection', (err, promise) => {
   console.log(`Unhandled Rejection: ${err.message}`.red);
   // Close server and Exit process
   server.close(() => process.exit(1));
